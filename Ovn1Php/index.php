53783df6cbc6175a4163db6bc793b593fecdb7dd
elmblom <91oge4u8@duck.com>
2025-01-13 12:05:52 +0200

    init

---
+++ Ovn1Php/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+ 
+<?php
+$myschool = "prakticum";
+$number = 5;
+echo "<p>$myschool</p>";
+echo 4 * 2.5 ;
+echo "<br><a href='https://prakticum.fi'>Prakticums sida</a>";
+?>
+
+</body>
+</html>
