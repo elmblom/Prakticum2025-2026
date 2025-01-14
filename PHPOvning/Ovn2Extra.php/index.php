d44de095f779f0dffdf6c27cea44fb2a1d6be573
elmblom <91oge4u8@duck.com>
2025-01-14 12:01:50 +0200

    Done ovn2 extra

--- PHPOvning/Ovn2Extra.php/index.php
+++ PHPOvning/Ovn2Extra.php/index.php
@@ -22,9 +22,19 @@ echo "<br>";
 echo file_get_contents($file);
 ?>
 <br>
+
 <form action="name.php" method="POST">
-Name: <input type="text" name="name"><br>
-E-mail: <input type="text" name="email"><br>
+Namn: <input type="text" name="name"><br>
+Ålder: <input type="text" name="age"><br>
+Nummer 1: <input type="number" name="number1"><br>
+Nummer 2: <input type="number" name="number2"><br>
+Operation: <select type="dropdown" name="operation">
+<option value="+">+</option>
+<option value="-">-</option>
+<option value="*">*</option>
+<option value="/">/</option>
+</select>
+<br>
 <input type="submit">
 </form>
 </body>
--- PHPOvning/Ovn2Extra.php/name.php
+++ PHPOvning/Ovn2Extra.php/name.php
@@ -1,8 +1,46 @@
+<?php
+$day = "Monday";
+$numbers = [6, 9, 3, 10, 5];
+$answer = $_POST['number1'] + $_POST['number2'];
+?>
 <html>
 <body>
 
-Welcome <?php echo $_POST["name"]; ?><br>
-Your email address is: <?php echo $_POST["email"]; ?>
-
+Välkommen <?php echo $_POST["name"]; ?><br>
+Du är <?php echo $_POST["age"]; ?> år gammal.<br>
+<br>
+<?php
+echo "Today is ";
+switch ($day) {
+    case "Monday":
+        echo "Meds Monday";
+        break;
+    case "Tuesday":
+        echo "Teriffic Tuesday";
+        break;
+    case "Wednesday":
+        echo "Wacky Wednesday";
+        break;
+    case "Thursday":
+        echo "Thirstyday";
+        break;
+    case "Friday":
+        echo "Freestyle Friday";
+        break;
+    case "Saturday":
+        echo "Sad Saturday";
+        break;
+    case "Sunday":
+        echo "Sunny Sunday";
+        break;
+    default:
+        echo "Invalid day";
+};
+echo "<br><br>";
+sort($numbers);
+echo implode(", ", $numbers);
+echo "<br><br>";
+echo "$_POST[number1] $_POST[operation] $_POST[number2] = $answer" ;
+?>
 </body>
 </html>
